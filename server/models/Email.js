d3ba9c2552ef03bb9a4dc6c2b81ab102c195e90a
nsLittle <notsolittle88@gmail.com>
2025-03-28 17:59:30 -0400

    email schema fixed

--- server/models/Email.js
+++ server/models/Email.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const EmailSchema = new mongoose.Schema({
   recipient: { type: String, required: true },
   subject: { type: String, required: true },
-  body: { type: String, required: true },
+  html: { type: String, required: true },
   sendAt: { type: Date, required: true },
   status: { type: String, enum: ["pending", "sent"], default: "pending" },
 });
