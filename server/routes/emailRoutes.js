916023892dd1f2dc60a7721ed0c4d6b366d903fc
nsLittle <notsolittle88@gmail.com>
2025-03-28 17:39:14 -0400

    found the offending test route throwing everything off

--- server/routes/emailRoutes.js
+++ server/routes/emailRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const {
   getScheduledEmails,
-  sendTestEmail,
   triggerEmailRequest,
 } = require("../controllers/emailController");
 const { protect } = require("../middlewares/authMiddleware");
@@ -9,12 +8,6 @@ const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.get("/scheduled", protect, getScheduledEmails);
-
-router.post("/send-test", protect, sendTestEmail);
-
-router.post(
-  "/:username/:habit_id/trigger-email-request",
-  emailController.triggerEmailRequest
-);
+router.post("/:username/:habit_id/trigger-email-request", triggerEmailRequest);
 
 module.exports = router;
