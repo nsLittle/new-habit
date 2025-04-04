f7baf64383e7cbae3f426dfd2d63e4a9f14c654b
nsLittle <notsolittle88@gmail.com>
2025-04-04 16:00:31 -0400

    feedback route order fixed

--- server/routes/feedbackRoutes.js
+++ server/routes/feedbackRoutes.js
@@ -12,8 +12,9 @@ const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.get("/aggregate/:habit_id", protect, getAggregateFeedback);
-router.post("/:username/:habit_id/:teammemberId", protect, submitFeedback);
 router.get("/:username/:habit_id", protect, getFeedback);
+
+router.post("/:username/:habit_id/:teammemberId", protect, submitFeedback);
 router.patch("/:username/:habit_id/rating", protect, editFeedbackRating);
 router.patch(
   "/:username/:habit_id/thanks-rating",
