05a0a1c37b6c7cf1985b897b0a837fc2d062831d
nsLittle <notsolittle88@gmail.com>
2025-04-08 12:02:23 -0400

    debugging still in progress

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -77,6 +77,7 @@ exports.passwordResetRequest = async (req, res) => {
   console.log("Request body:", req.body);
   const { email } = req.body;
   console.log("Extracted email:", email);
+  console.log("✅ Checking DB connection:", mongoose.connection.readyState);
 
   try {
     const user = await User.findOne({ email });
